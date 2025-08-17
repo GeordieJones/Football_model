f3040e702693fb92c37ea79e54be006f05843109
Geordie Jones <geordiejonesnufc@icloud.com>
2025-08-17 14:58:49 -0500

    final set up with matplotlib

--- gameboard.py
+++ gameboard.py
@@ -7,6 +7,7 @@ class Player:
         self.team = team
         self.number = number
         self.label = label
+        self.route = []
 
     def set_position(self, x, y):
         self.x = x
@@ -15,6 +16,10 @@ class Player:
     def move(self, dx, dy):
         self.x += dx
         self.y += dy
+
+    def set_route(self, route_points):
+        """Route is a list of (x, y) tuples"""
+        self.route = route_points
 
 class Field:
     def __init__(self, length=120, width=53.3):
@@ -35,6 +40,9 @@ class Field:
             color = 'blue' if p.team == "offense" else 'red'
             plt.scatter(p.x, p.y, c=color, s=200)
             plt.text(p.x, p.y+1, str(p.number), ha='center')
+            if p.route:
+                rx, ry = zip(*([(p.x, p.y)] + p.route))  # start at player position
+                plt.plot(rx, ry, color=color, linestyle="--")
 
         plt.xlim(0, self.length)
         plt.ylim(0, self.width)
@@ -43,23 +51,43 @@ class Field:
 # Example usage
 players = [
     # Offense
-    Player(10, 26, "offense", 12, "C"),    #OL 1-5
-    Player(9, 24, "offense", 80, "OL"),
-    Player(8, 22, "offense", 81, "OL"),
-    Player(9, 28, "offense", 52, "OL"),
-    Player(8, 30, "offense", 24, "OL"),
+    Player(25, 26, "offense", 12, "C"),    #OL 1-5
+    Player(24, 24, "offense", 80, "OL"),
+    Player(23, 22, "offense", 81, "OL"),
+    Player(24, 28, "offense", 52, "OL"),
+    Player(23, 30, "offense", 24, "OL"),
 
-    Player(10, 45, "offense", 81, "WR"),
-    Player(10, 5, "offense", 52, "WR"),   #WR 6-8
-    Player(10, 15, "offense", 24, "WR"),
+    Player(25, 45, "offense", 81, "WR"),
+    Player(25, 5, "offense", 52, "WR"),   #WR 6-9
+    Player(25, 15, "offense", 24, "WR"),
+    Player(25, 35, "offense", 24, "WR"),
+
+    Player(20, 26, "offense", 52, "WR"),   #QB
+    Player(20, 28, "offense", 24, "WR"),   #RB
+
 
     # Defense
-    Player(12, 26, "defense", 52, "LB"),   # LB in the box
-    Player(20, 15, "defense", 24, "CB"),   # CB covering WR bottom
-    Player(20, 37, "defense", 25, "CB"),   # CB covering WR top
-    Player(25, 20, "defense", 30, "S"),    # Safety bottom
-    Player(25, 32, "defense", 31, "S"),    # Safety top
+    Player(28, 30, "defense", 12, "DT"),    #OL 1-5
+    Player(28, 24, "defense", 80, "DT"),
+    Player(28, 22, "defense", 81, "DT"),
+    Player(28, 28, "defense", 52, "DE"),
+
+    Player(35, 25, "defense", 52, "LB"),
+
+
+
+    Player(28, 45, "defense", 81, "WR"),
+    Player(28, 5, "defense", 52, "WR"),   #WR 6-9
+    Player(28, 15, "defense", 24, "WR"),
+    Player(28, 35, "defense", 24, "WR"),
+
+    Player(45, 35, "defense", 35, "WR"),   #safteys
+    Player(45, 15, "defense", 15, "WR")
 ]
+
+players[5].set_route([(35, 45), (45, 45)])   # WR go route
+players[6].set_route([(30, 20), (35, 25)])   # WR slant
+players[9].set_route([(18, 26)])   
 
 field = Field()
 field.draw(players)
